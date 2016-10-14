0858a0e0aeac4b6964c715d43373d77aef13a37d
sareduhh <sarahe.kaminski@gmail.com>
2016-10-14 11:00:49 -0400

    first commit

---
+++ fortune-teller-mvc/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace fortune_teller_mvc.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<fortune_teller_mvc.Models.FortuneTellerMVCEntities>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(fortune_teller_mvc.Models.FortuneTellerMVCEntities context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
